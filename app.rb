4891837bbbd04cac100c1a2ba9a46f67a470b3c2
Nacho <gicanoi@gmail.com>
2016-09-03 14:47:42 +0200

    ganar, ingresar letra

--- app.rb
+++ app.rb
@@ -4,5 +4,15 @@ require './lib/hangman'
 get '/' do
     @@hangman = Hangman.new
     @secret = @@hangman.getSecretWord
+    @gano = @@hangman.gano
     erb :index
 end
+
+post '/letra' do
+    @@hangman.ingresarLetra params[:letra] 
+    @secret = @@hangman.getSecretWord
+    @gano = @@hangman.gano
+    erb :index
+end
+
+
