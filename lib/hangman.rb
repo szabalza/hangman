2590b44dd5619d8f3fb26968d416319a7ee06b38
Nacho <gicanoi@gmail.com>
2016-09-03 14:57:14 +0200

    gano

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,19 @@
 class Hangman
     def initialize
         @palabra = "hangman"
-        @letrasConocidas = []
+        @letras = ""
     end
     def getSecretWord 
-        "_ _ _ _ _ _ _"
+        filtered = (@palabra.split '').map {|c| if @letras.include? c then c else '_' end}
+        return filtered.join ' '
+    end
+    def ingresarLetra l
+        @letras = @letras << l
+    end
+    def getLetras 
+        @letras
+    end
+    def gano 
+         getSecretWord.gsub('_','').gsub(' ','') == @palabra
     end
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -8,4 +8,23 @@ describe Hangman do
         palabra =  @hangman.getSecretWord
         expect(palabra).to be == "_ _ _ _ _ _ _"
     end
+    it 'Al ingresar la letra "a" la las letras conocidas son "a"' do
+        @hangman.ingresarLetra "a"
+        letras =  @hangman.getLetras
+        expect(letras).to be == "a"
+    end
+    it 'Al ingresar la letra "a" la palabra secreta retorna "_ a _ _ _ a _"' do
+        @hangman.ingresarLetra "a"
+        palabra =  @hangman.getSecretWord
+        expect(palabra).to be == "_ a _ _ _ a _"
+    end
+    it 'Al ingresar las letras de "hangman" gano es "true"' do
+        @hangman.ingresarLetra "a"
+        @hangman.ingresarLetra "n"
+        @hangman.ingresarLetra "m"
+        @hangman.ingresarLetra "h"
+        @hangman.ingresarLetra "g"
+        gano = @hangman.gano
+        expect(gano).to be == true
+    end
 end
